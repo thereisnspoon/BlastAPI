939cf4746691c83bb76ac595074c0031b481ed28
ImStaked <82274336+ImStaked@users.noreply.github.com>
2022-12-18 01:43:41 -0600

    UNTESTED!!!!
    Updated

--- Houston - Testnet/node_setup/Moonriver_Node_Setup.sh
+++ Houston - Testnet/node_setup/Moonriver_Node_Setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-
 # Moonriver Node Setup
 # by ImStaked
 
 # Settings
-RELEASE_VERSION=v0.27.2
-
+RELEASE_VERSION=v0.28.1
+BINARY_URL="https://github.com/PureStake/moonbeam/releases/download/${RELEASE_VERSION}/moonbeam"
+DATA_DIR="/var/lib/moonriver-data"
 
 function setup_system {
-     apt update && apt upgrade -y
-     sudo apt-get install htop net-tools nmon nload iotop zip unzip git curl wget clang jq net-tools lz4 -y
+     # Update System
+     sudo apt-get update && sudo apt-get upgrade -y 
+     sudo apt-get autoremove -y && sudo apt-get autoclean -y
+     sudo apt-get install git curl wget jq net-tools lz4 zip unzip mlocate unattended-upgrades net-tools whois apt-transport-https ca-certificates libssl-dev gnupg bash-completion needrestart node-ws -y
+
      adduser moonriver_service --system --no-create-home
-     mkdir -p /var/lib/moonriver-data
-     cd /var/lib/moonriver-data
-     wget https://github.com/PureStake/moonbeam/releases/download/v0.27.2/moonbeam
-     chmod +x /var/lib/moonriver-data/moonriver && chown -R moonriver_service /var/lib/moonriver-data/
+     mkdir -p $DATA_DIR && cd $DATA_DIR
+     wget $BINARY_URL
+     chmod +x /var/lib/moonriver-data/moonbeam && chown -R moonriver_service $DATA_DIR
 }
 
 function create_service {
@@ -31,42 +33,58 @@ function create_service {
      SyslogIdentifier=moonriver
      SyslogFacility=local7
      KillSignal=SIGHUP
-     ExecStart=/var/lib/moonriver-data/moonriver \
-          --port 33333 \
-          --rpc-port 9933 \
-          --ws-port 9944 \
+     ExecStart=/var/lib/moonriver-data/moonbeam \
+          --sync=full \
+          --blocks-pruning=archive \
+          --port 30555 \
+          --rpc-port 9955 \
+          --ws-port 9966 \
           --execution wasm \
           --wasm-execution compiled \
-          --state-pruning=1000 \
-          --trie-cache-size 0 \
-          --db-cache 8000 \
+          --rpc-cors all \
           --unsafe-rpc-external \
           --unsafe-ws-external \
+          --prometheus-external \
           --prometheus-port 9102 \
-          --rpc-cors all \
           --base-path /var/lib/moonriver-data \
           --chain moonriver \
-          --name "ImStaked" \
+          --name "<NODE_NAME>" \
           -- \
           --port 33334 \
-          --rpc-port 9934 \
-          --ws-port 9945 \
-          --prometheus-port 9103 \
-          --rpc-cors all \
           --execution wasm \
-          --state-pruning=1000 \
-          --name="ImStaked (Embedded Relay)"
+          --pruning=1000 \
+          --prometheus-external \ 
+          --prometheus-port 9616 \
+          --name="<NODE_NAME>-embedded-relay"
      [Install]
      WantedBy=multi-user.target
 EOF
 }
-
 setup_system
 create_service
-
-
-chown -R moonriver_service /var/lib/moonriver-data/
-
-
+ 
+# Enable and Start the service
+sudo systemctl daemon-reload
+sudo systemctl enable moonriver.service
+sudo systemctl start moonriver.service
+sudo systemctl stop moonriver.service 
+# Polkadot Data Snapshot
+rm -rf "$DATA_DIR"/polkadot
+mkdir -p "$DATA_DIR"/polkadot/chains/polkadot
+echo "** Downloading and extracting chain data this could take a while **"
+curl -o - -L https://storage.googleapis.com/polkadot_snapshots/polkadot.RocksDB.tar | lz4 -c -d - | tar -x -C "$DATA_DIR"/polkadot/chains/polkadot
+# Moonriver Data Snapshot
+rm -rf "$DATA_DIR"/chains/moonriver
+mkdir -p "$DATA_DIR"/chains/moonriver
+echo "** Downloading and extracting chain data this could take a while **"
+# Sorry old snapshot is better than no snapshot probably
+curl -o - -L https://snapshots.stakecraft.com/moonriver_2022-04-30.tar | tar -x -C /data/chains/moonriver
+# Chown After Moving All data
+sudo chown -R moonriver_service "$DATA_DIR"
 systemctl start moonriver
 systemctl enable moonriver
+STATUS=$(systemctl status moonriver)
+echo $STATUS 
+echo "Script is complete"
+echo '*  Check the service status with --->     sudo systemctl status moonriver.service  *'
+echo '*  Check the service logs --->            sudo journalctl -f -u moonriver.service  *'
