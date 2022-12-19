e458b382173ce3ec1e509207576ce4260e56cb59
ImStaked <82274336+ImStaked@users.noreply.github.com>
2022-12-19 03:35:54 -0600

    Moons added

--- node_monitor.sh
+++ node_monitor.sh
@@ -4,7 +4,7 @@
 # by ImStaked
 
 # Osmosis Health Check
-OSMO_NODE_URL='http://10.204.53.103:26657/status'
+OSMO_NODE_URL='http://IP:PORT/status'
 osmosis_catchup_status=$(curl -s $OSMO_NODE_URL | jq .result.sync_info.catching_up)
 if [ "$osmosis_catchup_status" != false ]
 then
@@ -14,7 +14,7 @@ echo "Osmosis Is Healthy"
 fi
 
 # Gnosis Health Check
-GNOSIS_NODE_URL='http://10.204.53.150:8545/status'
+GNOSIS_NODE_URL='http://IP:PORT/status'
 gnosis_catchup_status=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_syncing","params": []}' $GNOSIS_NODE_URL | jq .result )  
 if [ "$gnosis_catchup_status" != false ]
 then
@@ -24,7 +24,7 @@ echo "Gnosis Is Health"
 fi
 
 # Eth Health Check
-ETHEREUM_NODE_URL='http://10.204.53.49:8545'
+ETHEREUM_NODE_URL='http://IP:PORT'
 ethereum_catchup_status=$(curl -s -H "Content-Type: application/json" -d '{"id":1, "jsonrpc":"2.0", "method": "eth_syncing","params": []}' $ETHEREUM_NODE_URL | jq .result )
 if [ "$ethereum_catchup_status" != false ]
 then
@@ -34,7 +34,7 @@ echo "Ethereum Is Healthy"
 fi
 
 # Avalanche Health Check
-AVAX_NODE_URL='http://10.204.53.246:9650/ext/health'
+AVAX_NODE_URL='http://1P:PORT/ext/health'
 avax_catchup_status=$(curl -s $AVAX_NODE_URL | jq .healthy )
 if [ "$avax_catchup_status" != true ]
 then
@@ -44,7 +44,7 @@ echo "Avalanche Is Healthy"
 fi
 
 # Astar Health Check
-ASTAR_NODE_URL='http://10.204.53.149:9933/health'
+ASTAR_NODE_URL='http://IP:PORT/health'
 astar_catchup_status=$(curl -s $ASTAR_NODE_URL | jq .isSyncing )
 if [ "$astar_catchup_status" != false ]
 then
@@ -54,7 +54,7 @@ echo "Astar Is Healthy"
 fi
 
 # Shiden Health Check
-SHIDEN_NODE_URL='http://10.204.53.149:9933/health'
+SHIDEN_NODE_URL='http://IP:PORT/health'
 shiden_catchup_status=$(curl -s $SHIDEN_NODE_URL | jq .isSyncing )
 if [ "$shiden_catchup_status" != false ]
 then
@@ -63,3 +63,23 @@ else
 echo "Shiden Is Healthy"
 fi
 
+
+# Moonbeam Health Check
+MOONBEAM_NODE_URL='http://IP:PORT/health'
+moonbeam_catchup_status=$(curl -s $MOONBEAM_NODE_URL | jq .isSyncing )
+if [ "$moonbeam_catchup_status" != false ]
+then
+echo "$moonbeam_catchup_status"
+else
+echo "Moonbeam Is Healthy"
+fi
+
+# Moonriver Health Check
+MOONRIVER_NODE_URL='http://IP:PORT/health'
+moonriver_catchup_status=$(curl -s $MOONRIVER_NODE_URL | jq .isSyncing )
+if [ "$moonriver_catchup_status" != false ]
+then
+echo "$moonriver_catchup_status"
+else
+echo "Moonriver Is Healthy"
+fi
