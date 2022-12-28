0d942eeddd6150e1dcf63a8261e7aecbdf600051
ImStaked <82274336+ImStaked@users.noreply.github.com>
2022-12-28 12:46:34 -0600

    Update whitelist_blast_ips.sh

--- whitelist_blast_ips.sh
+++ whitelist_blast_ips.sh
@@ -20,8 +20,12 @@ ipset add whitelist 52.20.248.136
 ipset add whitelist 52.0.232.1
 ipset add whitelist 34.194.155.195
 
-# Add the whitelist to the firewall
+# Example - only permit access to 1 port
+# iptables -A INPUT -p tcp -m tcp 8545 -m set --match-set whitelist src -j ACCEPT
+
+# Allow whitelist full access
 iptables -A INPUT -m set --match-set whitelist src -j ACCEPT
+
 
 # Save lists and make persistant
 apt install ipset-persistent iptables-persistent -y
@@ -29,4 +33,4 @@ ipset save whitelist -f ipset-whitelist.backup
 iptables-save > /etc/iptables/rules.v4
 
 
-echo "The whitelist and associated firewall rule have been configured and should persist after reboot"
+echo "All blastapi IP addresses have been whitelisted"
