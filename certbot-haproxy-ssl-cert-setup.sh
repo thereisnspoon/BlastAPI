a929b9694dd9282ffe7a10994f55d7a9abb56978
ImStaked <82274336+ImStaked@users.noreply.github.com>
2022-12-28 10:12:26 -0600

    Untested 1st draft

--- certbot-haproxy-ssl-cert-setup.sh
+++ certbot-haproxy-ssl-cert-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Snapd automatically handles renewing the certificate
 echo "Please enter your fully qualified domain name or a comma seperated list of domains"
 read DOMAIN
 
@@ -17,5 +18,17 @@ sudo mkdir -p /etc/ssl/"$DOMAIN"
 sudo cat /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem \
     /etc/letsencrypt/live/"$DOMAIN"m/privkey.pem \
     | sudo tee /etc/ssl/"$DOMAIN"/"$DOMAIN".pem
-    
-    
+   
+   
+# Snap should renew the cert automatically
+# Create Script that runs daily
+touch /opt/update-certs.sh
+cat > /opt/update-certs.sh <<EOF
+bash -c "cat /etc/letsencrypt/live/"$DOMAIN"/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/ssl/$DOMAIN/$DOMAIN"
+service haproxy reload
+EOF
+chmod +x /opt/update-certs.sh
+
+# Schedule script to run daily
+touch /etc/cron.d/cert-update
+echo "0 0 * * * root bash /opt/update-certs.sh" > /etc/cron.d/cert-update
